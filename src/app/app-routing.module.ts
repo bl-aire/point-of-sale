4bde05d013c97f3781c09e112faedc3de016a567
Blaire <ogoh_blessing@yahoo.com>
2022-05-21 23:10:56 +0100

    Set up router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './_shared/components/header/header.component';
+import { HomeComponent } from './home/home.component';
+import { OrderComponent } from './order/order.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'order', component: OrderComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
